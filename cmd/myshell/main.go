fecca24bb88e6d3a67dcbe20012ff4fda92258f6
Maarten Ackermans <4571935+mackermans@users.noreply.github.com>
2024-06-14 10:10:34 +0200

    exit status code

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,25 +4,43 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+func handleExitCommand(args []string) {
+	var err error
+	statusCode := 0
+	if len(args) > 0 {
+		statusCode, err = strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("failed to parse exit status code:", err)
+			statusCode = 1
+		}
+	}
+	os.Exit(statusCode)
+}
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		rawInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			fmt.Println("failed to parse input: ", err)
+			fmt.Println("failed to parse input:", err)
 			break
 		}
 
-		trimmedInput := strings.TrimSpace(input)
-		if trimmedInput == "exit" {
-			break
-		}
+		trimmedInput := strings.TrimSpace(rawInput)
 
-		output := fmt.Sprintf("%s: command not found\n", trimmedInput)
-		fmt.Fprint(os.Stdout, output)
+		input := strings.Split(trimmedInput, " ")
+
+		switch command := input[0]; command {
+		case "exit":
+			handleExitCommand(input[1:])
+		default:
+			output := fmt.Sprintf("%s: command not found\n", command)
+			fmt.Fprint(os.Stdout, output)
+		}
 	}
 }
